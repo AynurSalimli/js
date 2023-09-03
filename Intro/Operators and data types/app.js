10a7fe63ab7420e73ec47452fb18318e75d1dc21
unknown <aynur.selimli@ascca.edu.az>
2023-09-03 20:49:04 +0400

    data types

---
+++ Intro/Operators and data types/app.js
@@ -0,0 +1,24 @@
+let value;
+const value1= 10;
+const value2= 8;
+
+value = value1+value2
+value = value1-value2
+value = value1*value2
+value = value1/value2
+value = value1 % value2
+value = Math.PI
+value = Math.E
+value = Math.ceil(3.2) //4
+value = Math.floor(3.8) //3
+value = Math.sqrt(16) //4
+value = Math.round(3.2) // 3
+value = Math.abs(-10) // 10
+value = Math.pow(2,8) //256
+value = Math.max(2, 7) //7
+value = Math.min(3, 8) //3
+value = Math.random() ;
+
+console.log(value);
+
+
