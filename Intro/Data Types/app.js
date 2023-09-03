c8bbc9fae25e978be1be516e39f5ebea50c70e03
unknown <aynur.selimli@ascca.edu.az>
2023-09-03 19:35:24 +0400

    data types

--- Intro/Data Types/app.js
+++ Intro/Data Types/app.js
@@ -6,10 +6,44 @@
 
 
 //number
-var a = 10; 
-var b = 3.14;
-console.log(typeof b);
+// var a = 10; 
+// var b = 3.14;
+// console.log(typeof b);
 
 //string
-var name = 'Aynur';
-console.log(typeof name);
+// var name = 'Aynur';
+// console.log(typeof name);
+
+//boolean
+// var a = true;
+// console.log(typeof a);
+
+//undefined
+// var a;
+// console.log(typeof a);
+
+// var numbers = [1, 2, 3, 4, 8]
+// console.log(numbers[1]);
+
+// var person = {
+//     name: "Aynur",
+//     surname: "Salim",
+//     age: 19
+// }
+
+//object type
+// console.log(person);
+// var date = new Date();
+// console.log(typeof date); 
+
+// var hello = function(){
+//     console.log('Heyyo');
+// }
+// console.log(typeof hello);
+
+//object type
+
+// var a = [1,2,3]
+// var b = a
+// a.push(4)
+// console.log(b);
