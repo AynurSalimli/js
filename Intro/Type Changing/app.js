82f201fcd41b905c59e1b15ecc227362f37fca5b
unknown <aynur.selimli@ascca.edu.az>
2023-09-03 20:23:01 +0400

    data types

---
+++ Intro/Type Changing/app.js
@@ -0,0 +1,15 @@
+//String-e cevirme
+// let value;
+// value = String(123);
+// value = String(true);
+// value=String([1.3,1.4])
+// value = (20).toString()
+
+//Number-a cevirme
+// value=Number("12") //Number
+// value=Number(undefined) //NaN
+// value = parseFloat("3.14")
+// value = parseInt("54")
+
+// const a = "Hello"-34;
+// console.log(typeof a);
