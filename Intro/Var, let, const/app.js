b4ca1d3b85d6a8dc4a01179b46b0427ef25efec9
unknown <aynur.selimli@ascca.edu.az>
2023-09-03 20:03:11 +0400

    data types

--- Intro/Var, let, const/app.js
+++ Intro/Var, let, const/app.js
@@ -1,9 +1,21 @@
 //var
 
-var a;
-a=20;
-console.log(a);
+// var a;
+// a=20;
+// console.log(a);
 
-var name = "Aynur Salim"
-var name = "Aynur"
-console.log(name);
+// var name = "Aynur Salim"
+// name = "Aynur"
+// console.log(name);
+
+//let 
+// let name="Aynur Salim"
+// name="Aynur"
+// console.log(name);
+
+//const
+// const a = [5];
+// a.push(4)
+// const b = 10;
+// b=20
+// console.log(b);
