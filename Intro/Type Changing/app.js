8b0f7a7f2a73bbbbe0109f8bb80bbdec6e4e3b05
unknown <aynur.selimli@ascca.edu.az>
2023-09-03 21:31:24 +0400

    Turning the types

--- Intro/Type Changing/app.js
+++ Intro/Type Changing/app.js
@@ -1,15 +1,17 @@
-//String-e cevirme
-// let value;
-// value = String(123);
-// value = String(true);
-// value=String([1.3,1.4])
-// value = (20).toString()
+// String-e cevirme
+let value;
+value = String(123);
+value = String(true);
+value=String([1.3,1.4])
+value = (20).toString()
 
-//Number-a cevirme
-// value=Number("12") //Number
-// value=Number(undefined) //NaN
-// value = parseFloat("3.14")
-// value = parseInt("54")
+// Number-a cevirme
+value=Number("12") //Number
+value=Number(undefined) //NaN
+value = parseFloat("3.14")
+value = parseInt("54")
 
-// const a = "Hello"-34;
-// console.log(typeof a);
+const a = "Hello"-34;
+const b = "Hello"+34;
+
+console.log(typeof a);
