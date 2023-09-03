fa35104d8bbd5f2aa1810090a188dbb0b35f3468
unknown <aynur.selimli@ascca.edu.az>
2023-09-03 21:32:17 +0400

    var let and const

--- Intro/Var, let, const/app.js
+++ Intro/Var, let, const/app.js
@@ -1,21 +1,21 @@
-//var
+// var
 
-// var a;
-// a=20;
-// console.log(a);
+var a;
+a=20;
+console.log(a);
 
-// var name = "Aynur Salim"
-// name = "Aynur"
-// console.log(name);
+var name = "Aynur Salim"
+name = "Aynur"
+console.log(name);
 
-//let 
-// let name="Aynur Salim"
-// name="Aynur"
-// console.log(name);
+// let 
+let name="Aynur Salim"
+name="Aynur"
+console.log(name);
 
-//const
-// const a = [5];
-// a.push(4)
-// const b = 10;
-// b=20
-// console.log(b);
+// const
+const a = [5];
+a.push(4)
+const b = 10;
+b=20
+console.log(b);
