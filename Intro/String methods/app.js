4c176db105bd221022d29e92cb781ab7a48af352
unknown <aynur.selimli@ascca.edu.az>
2023-09-03 21:28:30 +0400

    String types

---
+++ Intro/String methods/app.js
@@ -0,0 +1,23 @@
+let value;
+let langs = "Java,C++,Python"
+const firstName = "Louis"
+const lastName = "ArmStrong"
+
+value = firstName + lastName
+value = firstName.concat(" ", lastName," ","Caz") // Louis Armstrong Caz
+value = firstName.toLowerCase();
+value = firstName.toUpperCase();
+value = firstName[3] // i
+value = firstName[4] // s
+
+//Index Of
+value = firstName.indexOf("i") //3 
+value = firstName.indexOf("w") //-1
+value = firstName.charAt(0); //L
+
+//split
+value = langs.split(",")
+value = langs.replace("Python", "CSS")
+value = langs.includes("Python")
+
+console.log(value);
