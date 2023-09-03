c1de772618c943984b6036eac1b2b1112284623b
unknown <aynur.selimli@ascca.edu.az>
2023-09-04 00:23:50 +0400

    Objects

--- Intro/Objects/app.js
+++ Intro/Objects/app.js
@@ -1,24 +1,34 @@
 let value;
 const programmer = {
-    name: "Aynur",
+  name: "Aynur",
+  age: 20,
+  email: "aynurs@gmail.com",
+  langs: ["Javascript", "Python", "C#"],
+  address: {
+    country: "Azerbaijan",
+    city: "Baku",
+  },
+  work: function () {
+    console.log("Programmer is working");
+  },
+};
+
+value = programmer;
+value = programmer.email;
+value = programmer["email"];
+value = programmer.langs;
+value = programmer.address.city;
+value = programmer.work();
+
+const programmers = [
+  {
+    name: "Aynur Salim",
     age: 20,
-    email: "aynurs@gmail.com",
-    langs: ["Javascript", "Python", "C#"],
-    address: {
-        country: 'Azerbaijan',
-        city: 'Baku',
-    },
-    work: function(){
-        console.log("Programmer is working");
-    }
-}
-
-
-
-
-value = programmer
-value = programmer.email
-value = programmer["email"]
-value = programmer.langs
-
+  },
+  {
+    name: "Jennie Williams",
+    age: 22,
+  },
+];
+value = programmers;
 console.log(value);
