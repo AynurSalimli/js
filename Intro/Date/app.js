6bd31b3020afa13aeedc0a7cebeebd42fc20969f
unknown <aynur.selimli@ascca.edu.az>
2023-09-05 00:32:19 +0400

    Date and time

---
+++ Intro/Date/app.js
@@ -0,0 +1,13 @@
+const date = new Date();
+const date1 = new Date("9-27-2003 06:15:30");
+const date2 = new Date("9/27/2003");
+const date3 = new Date("September 27 2003")
+const month1= date1.getMonth();
+const week1 = date1.getDate();
+const month2 = date1.getHours();
+const month3 = date1.getMonth();
+const month4 = date1.getMinutes();
+const month5 = date1.getSeconds();
+
+
+console.log(month5);
