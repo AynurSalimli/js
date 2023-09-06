4b7fea744fb5841155013e8c285f644cdba77496
unknown <aynur.selimli@ascca.edu.az>
2023-09-06 14:34:13 +0400

    ternary operators

---
+++ Intro/Ternary/app.js
@@ -0,0 +1,20 @@
+//==
+// ===
+// !=
+// !==
+// >
+// <
+// >=
+// <=
+console.log(2 == 2); //true
+console.log("js" == "javascript"); //false
+console.log(2 == "2"); //true
+console.log(2 === "2"); //false
+console.log(2>4); //false
+console.log(2 !== "2"); //true
+console.log(!(2>4)); //true
+console.log(!(3>=3)); //false
+//AND(all should be true = true)
+console.log((2!=2) && (4>2));
+//OR (one of them should be true = true)
+console.log((2 ===3) || (4>2));
