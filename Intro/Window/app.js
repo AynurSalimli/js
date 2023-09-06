c1d9f408bbfd09a99309cbe386fb23a7b6ff77ed
unknown <aynur.selimli@ascca.edu.az>
2023-09-06 17:36:29 +0400

    window

---
+++ Intro/Window/app.js
@@ -0,0 +1,37 @@
+// console.log(this);
+// console.log(window);
+// console.log(alert("hey"));
+// const a = confirm("U sure?")
+// console.log(a); //true
+// if (a) {
+//     console.log("delete");
+// }
+// else{
+//     console.log("don't delete");
+// }
+
+// const a = prompt("2 + 2 == ?");
+// console.log(a); //type = string
+// if (a == 4) {
+//   console.log("correct");
+// } else {
+//   console.log("not correct");
+// }
+
+let value;
+value = window;
+value = window.location;
+value = window.location.host;
+value = window.location.hostname;
+value = window.location.port;
+value = window.location.href;
+value = window.outerWidth;
+value = window.innerHeight;
+value = window.outerHeight;
+value = window.innerWidth;
+// if (confirm("Sayfa yenilensinmi?")) {
+//   window.location.reload();
+// } else {
+//   console.log("Hayir yenilenmesin");
+// }
+console.log(value);
