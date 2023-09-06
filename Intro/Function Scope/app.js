5bedd2f84cc205da04f8f16bc10efaadaadd00bf
unknown <aynur.selimli@ascca.edu.az>
2023-09-06 20:14:22 +0400

    Scope

---
+++ Intro/Function Scope/app.js
@@ -0,0 +1,25 @@
+//Scope
+
+// var value1 = 10;
+// var value2 = 20;
+// var value3 = 30;
+// function Func() {
+  var a = 40;
+  var b = 50;
+  const c = 60;
+
+//   console.log(value1, value2, value3);
+// }
+// Func();
+
+if (true) {
+  var a = 10;
+  let b = 20;
+  const c = 30;
+  console.log(a, b, c);
+}
+console.log(a);
+console.log(b);
+console.log(c);
+
+console.log(a,b,c);
