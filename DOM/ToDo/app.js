0c20647c6380fc8490f3793655db441b94c02c66
unknown <aynur.selimli@ascca.edu.az>
2023-09-07 13:42:36 +0400

    dom

--- DOM/ToDo/app.js
+++ DOM/ToDo/app.js
@@ -1,1 +1,22 @@
-console.log(window);
+let value;
+value = document.all.length;
+value = document.all[0];
+value = document.all[document.all.length - 1];
+
+const elements = document.all;
+for (let i = 0; i < elements.length; i++) {
+  console.log(elements[i]);
+}
+
+const collections = Array.from(document.all);
+collections.forEach(function (collection) {
+  console.log(collection);
+});
+
+value = document.body;
+value = document.head;
+value = document.location.hostname; //127.0.0.1
+value = document.URL;
+value = document.characterSet;
+console.log(value);
+// console.log(this.document);
