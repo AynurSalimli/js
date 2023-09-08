1cb1f3c03659c21962a93117fbb71cbb19dffecd
AynurSalimli <aynur.selimli@ascca.edu.az>
2023-09-08 21:10:30 +0400

    Arrow function

---
+++ ES6/Arrowfunc/app.js
@@ -0,0 +1,26 @@
+// const hey = function(){
+//     console.log('salam');
+// }
+
+// hey()
+
+//Arrow function
+// const hey = () => {
+//   console.log("salam");
+// };
+
+// hey()
+
+// const hey = (firstname) => {
+//   console.log("Hello", firstname);
+// };
+
+// hey("Aynur");
+
+// const cube = function (x) {
+//   return x * x * x;
+// };
+// console.log(cube(3));
+
+const cube = (x) =>  x * x * x;
+console.log(cube(3));
