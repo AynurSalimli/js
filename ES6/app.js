ce52462664347c597582d5c77653bef134fbd7e7
AynurSalimli <aynur.selimli@ascca.edu.az>
2023-09-08 21:07:07 +0400

    ES-6

---
+++ ES6/app.js
@@ -0,0 +1,25 @@
+// const hey = function(){
+//     console.log('salam');
+// }
+
+// hey()
+
+//Arrow function
+// const hey = () => {
+//   console.log("salam");
+// };
+
+// hey()
+
+// const hey = (firstname) => {
+//   console.log("Hello", firstname);
+// };
+
+// hey("Aynur");
+
+// const cube = function (x) {
+//   return x * x * x;
+// };
+// console.log(cube(3));
+
+const
